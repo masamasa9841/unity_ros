b25e2f070fd72f70b7031610334794d039209110
MasayaOkawa <masamasa9841@gmail.com>
2017-05-08 22:05:53 +0900

    test program

---
+++ subscribe_test.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+
+[System.Serializable]
+public class RosData
+{
+	public string op;
+	public string topic;
+}
+
+public class subscribe_test : MonoBehaviour {
+	WebSocket ws;
+
+	// Use this for initialization
+	void Start () {
+		ws = new WebSocket("ws://192.168.22.173:9090/");
+		//接続完了
+		ws.OnOpen += (sender, e) =>
+		{
+			Debug.Log("WebSocket Open");
+
+			RosData data = new RosData ();
+			data.op = "subscribe";
+			data.topic = "/camera/rgb/image_raw";
+			string json = JsonUtility.ToJson(data);
+			ws.Send(json);
+		};
+
+		ws.OnError += (sender, e) =>
+		{
+			Debug.Log("WebSocket Error Message: " + e.Message);
+		};
+
+		ws.OnClose += (sender, e) =>
+		{
+			Debug.Log("WebSocket Close");
+			RosData data = new RosData ();
+			data.op = "unsubscribe";
+			data.topic = "/camera/rgb/image_raw";
+			string json = JsonUtility.ToJson(data);
+			ws.Send(json);
+		};
+
+		ws.Connect();
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//受信したとき
+		ws.OnMessage += (sender, e) => {
+			string message = e.Data;
+		};
+		ws.OnClose += (sender, e) =>
+		{
+			Debug.Log("WebSocket Close");
+			RosData data = new RosData ();
+			data.op = "unsubscribe";
+			data.topic = "/camera/rgb/image_raw";
+			string json = JsonUtility.ToJson(data);
+			ws.Send(json);
+		};
+	}
+}
